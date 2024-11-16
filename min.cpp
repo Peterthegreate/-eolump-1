36027aae8f3445247f2ca9af60cbc72a75aaf4f5
Peterthegreate <greatpeter2010@gmail.com>
2024-11-16 11:09:48 +0100

    Create min.cpp

---
+++ min.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N, min;
+    cin >> N >> min;
+    cout << main << endl;
+    while(N>1){
+        cin >> a;
+        if(min>a) min = a;
+        N = N-1;
+    }
+    cout << min << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
