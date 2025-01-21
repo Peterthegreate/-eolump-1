d2e9b10474f205b6dedc3c93f87db7ccfdb75b5f
Peterthegreate <greatpeter2010@gmail.com>
2025-01-21 19:18:53 +0100

    Create задача 8863.cpp

---
+++ eolymp задачи/задача 8863.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;              
+
+int main()
+{
+    int N;
+    cin >> N;
+
+
+    if (N % 2 != 0) {
+    cout << 0 << endl;
+   } else {
+       cout << 1 << endl;
+   }
+    
+    return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
