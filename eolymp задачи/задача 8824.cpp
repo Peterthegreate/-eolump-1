660ed6725df9a2b824e718acc56edeb9003bc6b8
Peterthegreate <greatpeter2010@gmail.com>
2025-01-27 18:25:00 +0100

    Create задача 8824.cpp

---
+++ eolymp задачи/задача 8824.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int b,a,c;
+    
+    cin >> b >> a;
+    
+    if (a > 0)
+    {
+        c = a - 1;
+        cout << c << endl;
+    }
+    
+    else if (a < 0)
+    {
+        c = a + 1;
+        cout << c << endl; 
+    }
+    
+}
