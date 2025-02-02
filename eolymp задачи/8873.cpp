a6ade9783a962a777170c7471c4641fb56704d5c
Peterthegreate <greatpeter2010@gmail.com>
2025-02-02 10:47:33 +0100

    Create 8873.cpp

---
+++ eolymp задачи/8873.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    
+    cin >> a;
+    
+   
+    
+    
+    if (a < 10 and a > -10)
+    {
+        cout << "yes" << endl;
+    }
+    
+    else
+    {
+        cout << "no" << endl;
+    }
+   
+    
+    
+}
