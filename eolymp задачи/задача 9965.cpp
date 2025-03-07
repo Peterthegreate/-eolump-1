2896393a09014bb8a762b63eac860b71ebdcb5db
Peterthegreate <greatpeter2010@gmail.com>
2025-03-07 18:51:43 +0100

    Create задача 9965.cpp

---
+++ eolymp задачи/задача 9965.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    b = 1;
+    c = 1;
+    cin >> a;
+    while(b <= a){
+        cout << c;
+        cout << " ";
+        c = c + 2;
+        b = b + 1;
+    }
+    return 0;
+}
+    
+   
