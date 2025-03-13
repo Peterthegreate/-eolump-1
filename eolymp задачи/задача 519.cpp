2b3a01a4f2806eab97968e105753c4843fb9e9a6
Peterthegreate <greatpeter2010@gmail.com>
2025-03-13 16:49:12 +0100

    Create задача 519.cpp

---
+++ eolymp задачи/задача 519.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, a1, b1, M;
+    cin >> a >> b;
+    a1 = a * a;
+    b1 = b * b;
+    M = a1 + b1;
+    cout << M << endl;
+}
