1c01508276ccc7d991226a44ff41150d56b2bd87
Peterthegreate <greatpeter2010@gmail.com>
2025-03-15 09:37:43 +0100

    Create задача 8812.cpp

---
+++ eolymp задачи/задача 8812.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a, b, p, s;
+    cin >> a >> b;
+    p = 2*(a + b);
+    s = a * b;
+    
+    cout << p <<' '<< s << endl;
+}
