d52b0415229b09f2b6494e2a3304259bcef7fec5
Peterthegreate <greatpeter2010@gmail.com>
2025-04-03 18:47:46 +0200

    Create задача 8874.cpp

---
+++ eolymp задачи/задача 8874.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if (a > 9 and a < 100 or a < -9 and a > -100){
+        cout << "Ok" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
