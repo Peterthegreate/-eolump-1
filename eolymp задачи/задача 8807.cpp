44eec460ba2b08af66a77b41905a459bbf333fbd
Peterthegreate <greatpeter2010@gmail.com>
2025-04-05 11:58:15 +0200

    Create задача 8807.cpp

---
+++ eolymp задачи/задача 8807.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a, b;
+    cin >> a;
+    if(a < 0){
+        b = a * -1;
+        cout << b << endl;
+    } else{
+        b = a * -1;
+        cout << b << endl;
+    }
+}
