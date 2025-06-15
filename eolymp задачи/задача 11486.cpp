416fc1e85edfceaed20ff025e9e12629a9077c58
Peterthegreate <greatpeter2010@gmail.com>
2025-06-15 19:04:10 +0200

    Create задача 11486.cpp

---
+++ eolymp задачи/задача 11486.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n, b, c;
+    b = 0;
+    cin >> n;
+    if(n == 0 or n < 0){
+        cout << 0 << endl;
+    } else {
+        c = n - b;
+        cout << c << endl;
+    }
+}
